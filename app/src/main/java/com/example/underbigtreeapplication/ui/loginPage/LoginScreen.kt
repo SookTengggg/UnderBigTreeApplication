9f058af458a13d9a883b409908a92f9f9b8d0256
Jessssss <jesslinying@gmail.com>
2025-09-19 00:33:17 +0800

    put ubt_logo at signup,login,welcome screen

--- app/src/main/java/com/example/underbigtreeapplication/ui/loginPage/LoginScreen.kt
+++ app/src/main/java/com/example/underbigtreeapplication/ui/loginPage/LoginScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.underbigtreeapp.R
